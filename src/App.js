78c1b28715579987105d6a064c658f8da141849e
Elijah Prince <elijahprince73@gmail.com>
2022-10-06 15:01:11 -0700

    added code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import mockData from './data'
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const stall = async (stallTime = 3000) => {
+    await new Promise(resolve => setTimeout(resolve, stallTime));
+  }
+
+  useEffect( () => {
+    const fetchData = async () => {
+      await stall(3000)
+      setLoading(false)
+      setData(mockData)
+    }
+    fetchData()
+  }, [])
+
+  const calculateRewardPoints = (amount) => {
+    if (amount >=50 && amount < 100) {
+        return amount-50;
+    } else if (amount >100){
+        return (2*(amount-100) + 50);
+    }
+    return 0;
+  }
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='users'>
+          {data.map((user) => {
+              return (
+                <div>
+                  <p>{user.username}</p>
+                  <div className='transactions'>
+                    {user.transactions.map(({amount, month}) => {
+                      return (
+                        <div className='transactions-item'>
+                          <p>Month: {month}</p>
+                          <p>Total: ${amount}</p>
+                          <p>Reward Points: {calculateRewardPoints(amount)}</p>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+        </div>
       </header>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    username: 'Bob',
+    transactions:[
+      {
+        amount: 120,
+        month: 'August'
+      },
+      {
+        amount: 130,
+        month: 'May'
+      },
+      {
+        amount: 4999,
+        month: 'June'
+      }
+    ]
+  },
+  {
+    username: 'Rachel',
+    transactions:[
+      {
+        amount: 140,
+        month: 'August'
+      },
+      {
+        amount: 145,
+        month: 'October'
+      },
+      {
+        amount: 1020,
+        month: 'December'
+      }
+    ]
+  },
+  {
+    username: 'Sam',
+    transactions:[
+      {
+        amount: 140,
+        month: 'August'
+      },
+      {
+        amount: 145,
+        month: 'June'
+      },
+      {
+        amount: 1020,
+        month: 'January'
+      }
+    ]
+  }
+]
+
+export default data
